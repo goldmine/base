82359f4276b39b143293b6b172d7172fe10ffba5
aeon <goldmine@163.com>
2015-04-08 11:51:36 +0800

    add title

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   #get 'home/index'
 
   get 'home/help'
+  get 'home/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,14 +1,28 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "BaseApp"
+  end
+
   test "should get index" do
     get :index
     assert_response :success
+    assert_select "title", "首页 | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "帮助 | #{@base_title}"
   end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "关于 | #{@base_title}"
+  end
+
 
 end
